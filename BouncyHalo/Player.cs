89cba6b727137f9d32d727e98dd0bdb15eb393e4
Josh Hess <joshhess13@gmail.com>
2019-04-27 17:58:41 -0600

    Auto stash before merge of "master" and "origin/master"

--- BouncyHalo/Player.cs
+++ BouncyHalo/Player.cs
@@ -21,9 +21,14 @@ namespace BouncyHalo
         Vector2 flameLoffset;
         Vector2 flameRoffset;
         Vector2 flameScale;
-        float flameRotate;
 
-        public Player(float x, float y, ContentManager content )
+        int flameShuffleTimer;
+        int flameShuffleTime = 75;
+        bool showLeftFlame;
+        Vector2 engineOrigin;
+        float engineRotation;
+
+        public Player(float x, float y, ContentManager content)
         {
 
             pelican = content.Load<Texture2D>("pelican");
@@ -32,45 +37,68 @@ namespace BouncyHalo
             flameR = content.Load<Texture2D>("flameR");
             position = new Vector2(x, y);
             engineoffset = new Vector2(140, 165);
-            flameLoffset = new Vector2(70,160);
-            flameRoffset = new Vector2(60,160);
+            flameLoffset = new Vector2(70, 160);
+            flameRoffset = new Vector2(60, 160);
             flameScale = new Vector2(2, 2);
-            flameRotate = 0f;
+            engineRotation = 0f;
+            engineOrigin = new Vector2();
         }
+
         public void draw(SpriteBatch sb)
         {
             sb.Draw(pelican, position, Color.White);
-            sb.Draw(flameL, position + flameLoffset, null, null, null, flameRotate, flameScale, null, SpriteEffects.FlipHorizontally, 0);
-            sb.Draw(flameR, position + flameLoffset, null, null, null, flameRotate, flameScale, null, SpriteEffects.FlipHorizontally, 0);
-            sb.Draw(engine, position +engineoffset, Color.White);
-            
+
+            if (showLeftFlame)
+                sb.Draw(flameL, position + flameLoffset, null, Color.White, engineRotation, engineOrigin, flameScale, SpriteEffects.FlipHorizontally, 0);
+            else
+                sb.Draw(flameR, position + flameLoffset, null, Color.White, engineRotation, engineOrigin, flameScale, SpriteEffects.FlipHorizontally, 0);
+
+            sb.Draw(engine, position + engineoffset, null, Color.White, engineRotation, engineOrigin, Vector2.One, SpriteEffects.FlipHorizontally, 0);
         }
-        public void update()
+
+        public void update(GameTime dt)
+        {
+            UpdatePosition();
+            UpdateFlameAnimation(dt);
+
+        }
+
+        public void UpdateFlameAnimation(GameTime dt)
+        {
+            flameShuffleTimer += dt.ElapsedGameTime.Milliseconds;
+            while(flameShuffleTimer >= flameShuffleTime)
+            {
+                flameShuffleTimer -= flameShuffleTime;
+                showLeftFlame = !showLeftFlame;
+            }
+        }
+
+        public void UpdatePosition()
         {
             position.Y += 1f;
             KeyboardState state = Keyboard.GetState();
-            if (state.IsKeyDown(Keys.Right) == true)
+            if (state.IsKeyDown(Keys.Right) || state.IsKeyDown(Keys.D))
             {
                 position.X += 10f;
             }
-            if (state.IsKeyDown(Keys.Left) == true)
+            if (state.IsKeyDown(Keys.Left) || state.IsKeyDown(Keys.A))
             {
                 position.X -= 10f;
             }
-            if (state.IsKeyDown(Keys.Up) == true)
+            if (state.IsKeyDown(Keys.Up) || state.IsKeyDown(Keys.W))
             {
                 position.Y -= 10f;
+                engineRotation = -0.785f;
             }
-                if (state.IsKeyDown(Keys.Down) == true)
+            if (state.IsKeyDown(Keys.Down) || state.IsKeyDown(Keys.S))
             {
                 position.Y += 10f;
+                engineRotation = 0.785f;
             }
-
-
-
+            if (!(state.IsKeyDown(Keys.Down) || state.IsKeyDown(Keys.S) || state.IsKeyDown(Keys.Up) || state.IsKeyDown(Keys.W)))
+                engineRotation = 0;
+            
         }
-
-
 
 
 
