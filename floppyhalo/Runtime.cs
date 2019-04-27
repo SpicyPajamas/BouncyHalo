ae596f1edecc14c0b878c837556c8acebe8bdd19
Josh Hess <joshhess13@gmail.com>
2019-04-27 15:13:34 -0600

    Added another project for desktop

---
+++ floppyhalo/Runtime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace floppyhalo
+{
+    class Runtime
+    {
+
+
+        public static void Main(string[] args)
+        {
+            using(var game = Game1())
+            {
+                
+            }
+        }
+    }
+}
